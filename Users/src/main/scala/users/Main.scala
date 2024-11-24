5f03ffe18ab12f9493cb48a2f7503aa9ef11c4c4
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-11-24 18:24:40 +0100

    Added port and host to uses service

--- Users/src/main/scala/users/Main.scala
+++ Users/src/main/scala/users/Main.scala
@@ -1,6 +1,8 @@
 package users
 
 import java.io.File
+import scala.sys
+import scala.util.Try
 import shared.technologies.persistence.FileSystemDatabaseImpl
 import shared.adapters.persistence.FileSystemRepositoryAdapter
 import users.domain.model.*
@@ -9,6 +11,14 @@ import users.adapters.presentation.HttpPresentationAdapter
 
 object Main extends App:
   val db = FileSystemDatabaseImpl(File("/Users/Alessandro/Desktop/users/db"))
-  val adapter = FileSystemRepositoryAdapter[Username, User](db, "user")
+  val adapter = FileSystemRepositoryAdapter[Username, User](db, "users")
   val usersService = UsersServiceImpl(adapter)
-  HttpPresentationAdapter.start(usersService)
+  val host = sys.env.get("HOST").getOrElse("0.0.0.0")
+  val port = (for
+    portString <- sys.env.get("PORT")
+    portInt <- Try(Option(portString.toInt)).getOrElse({
+      sys.error("PORT must be an integer"); None
+    })
+  yield (portInt)).getOrElse(8080)
+  HttpPresentationAdapter.startHttpServer(usersService, host, port)
+  println(s"Listening on $host:$port")
--- Users/src/main/scala/users/adapters/presentation/HttpPresentationAdapter.scala
+++ Users/src/main/scala/users/adapters/presentation/HttpPresentationAdapter.scala
@@ -1,46 +1,56 @@
 package users.adapters.presentation
 
+import scala.concurrent.Future
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
+import akka.http.scaladsl.model.StatusCodes.*
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.RootJsonFormat
 import spray.json.DefaultJsonProtocol.*
 import users.domain.model.*
 import users.domain.UsersService
-import users.adapters.presentation.dto.*
 
 object HttpPresentationAdapter:
 
   given RootJsonFormat[Username] = jsonFormat1(Username.apply)
   given RootJsonFormat[Credit] = jsonFormat1(Credit.apply)
   given RootJsonFormat[User] = jsonFormat2(User.apply)
-  given RootJsonFormat[RechargeCreditDTO] = jsonFormat2(RechargeCreditDTO.apply)
 
-  def start(usersService: UsersService): Unit =
+  def startHttpServer(
+      usersService: UsersService,
+      host: String,
+      port: Int
+  ): Future[ServerBinding] =
     implicit val system = ActorSystem(Behaviors.empty, "actor-system")
 
     val route =
-      concat(
-        pathPrefix("users"):
-          concat(
-            post:
-              entity(as[Username]) { username =>
-                usersService.registerUser(username) match
-                  case Left(value)  => complete("Username already in use")
+      pathPrefix("users"):
+        concat(
+          pathPrefix(Segment / "credit"): username =>
+            concat(
+              get:
+                usersService.checkCredit(Username(username)) match
+                  case Left(value)  => complete(NotFound, "User not found")
                   case Right(value) => complete(value)
-              }
-            ,
-            path(Segment): username =>
-              usersService.checkCredit(Username(username)) match
-                case Left(value)  => complete("User not found")
-                case Right(value) => complete(value)
-          )
-        ,
-        pathPrefix("auth"):
-          get:
-            complete("auth")
-      )
+              ,
+              post:
+                entity(as[Credit]) { credit =>
+                  usersService.rechargeCredit(Username(username), credit) match
+                    case Left(value)  => complete(NotFound, "User not found")
+                    case Right(value) => complete(value)
 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+                }
+            ),
+          post:
+            entity(as[Username]) { username =>
+              usersService.registerUser(username) match
+                case Left(value) =>
+                  complete(Conflict, "Username already in use")
+                case Right(value) => complete(value)
+            }
+        )
+
+    Http().newServerAt(host, port).bind(route)
