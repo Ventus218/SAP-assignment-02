6627c621514dbaa452ec768721c9440fa9b4ef45
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-11-25 22:40:31 +0100

    Add InMemoryMapDB and Adapter for future testing

---
+++ Shared/src/main/scala/shared/adapters/persistence/InMemoryRepositoryAdapter.scala
@@ -0,0 +1,56 @@
+package shared.adapters.persistence
+
+import scala.collection.JavaConverters.*
+import shared.ports.persistence.Repository
+import shared.ports.persistence.exceptions.*
+import shared.technologies.persistence.InMemoryMapDatabase
+import scala.util.Try
+
+class InMemoryRepositoryAdapter[ID, T](
+    private val db: InMemoryMapDatabase,
+    private val entityName: String
+) extends Repository[ID, T]:
+
+  private given Conversion[ID, String] with
+    def apply(x: ID): String = x.hashCode().toString()
+
+  val map = Try(db.getMap(entityName).asScala)
+    .recover({ case e: IllegalStateException =>
+      db.createMap(entityName).asScala
+    })
+    .get
+    .asInstanceOf[scala.collection.mutable.Map[String, T]]
+
+  override def getAll(): Iterable[T] = map.values
+
+  override def insert(id: ID, entity: T): Either[DuplicateIdException, Unit] =
+    map.contains(id) match
+      case true =>
+        Left(
+          DuplicateIdException(s"An $entityName with id $id already exists.")
+        )
+      case false =>
+        map.put(id, entity)
+        Right(())
+
+  override def update(id: ID, f: T => T): Either[NotInRepositoryException, T] =
+    map.updateWith(id)(_.map(f)) match
+      case None =>
+        Left(
+          NotInRepositoryException(
+            s"An $entityName with id $id does not exist."
+          )
+        )
+      case Some(value) => Right(value)
+
+  override def find(id: ID): Option[T] = map.get(id)
+
+  override def delete(id: ID): Either[NotInRepositoryException, Unit] =
+    map.remove(id) match
+      case None =>
+        Left(
+          NotInRepositoryException(
+            s"An $entityName with id $id does not exist."
+          )
+        )
+      case Some(value) => Right(())
