686b848fd17a002a31e1d991ff27286c3adfbeda
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-11-26 08:57:27 +0100

    Added all unit tests for EBikesService

--- EBikes/src/test/scala/ebikes/domain/EBikesServiceTests.scala
+++ EBikes/src/test/scala/ebikes/domain/EBikesServiceTests.scala
@@ -2,10 +2,10 @@ package ebikes.domain
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
-import org.scalatest.BeforeAndAfterEach
 import shared.adapters.persistence.InMemoryRepositoryAdapter
 import shared.technologies.persistence.InMemoryMapDatabaseImpl
 import ebikes.domain.model.*
+import ebikes.domain.errors.*
 import ebikes.ports.persistence.EBikesRepository
 
 class EBikesServiceTests extends AnyFlatSpec:
@@ -36,11 +36,29 @@ class EBikesServiceTests extends AnyFlatSpec:
   "find" should "retrieve no bike if not present" in new DoubleBikeService:
     service.find("blabla") shouldBe None
 
-  "find" should "retrieve a bike if present" in new DoubleBikeService:
+  it should "retrieve a bike if present" in new DoubleBikeService:
     val id = bikeIds.head
     val bike = service.find(id)
     bike.isDefined shouldBe true
     bike.get.id shouldBe id
+
+  "register" should "add a new bike" in new CleanService:
+    val id = "b1"
+    service.register(id, P2D(0, 0), V2D(0, 0))
+    service.find(id).isDefined shouldBe true
+
+  it should "add a new bike with provided data and speed set to 0" in new CleanService:
+    val id = "b1"
+    val location = P2D(1, 2)
+    val direction = V2D(3, 4)
+    service.register(id, location, direction)
+    service.find(id) shouldBe Some(EBike(id, location, direction, 0))
+
+  it should "not allow to register a new bike with an already existing id" in new DoubleBikeService:
+    val id = bikeIds.head
+    val res = service.register(id, P2D(0, 0), V2D(0, 0))
+    res.isLeft shouldBe true
+    res.left.get shouldBe a[EBikeIdAlreadyInUse]
 
   private given Conversion[String, EBikeId] with
     def apply(x: String): EBikeId = EBikeId(x)
