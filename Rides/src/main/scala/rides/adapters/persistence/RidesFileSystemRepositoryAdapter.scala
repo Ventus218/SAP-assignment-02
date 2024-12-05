a208a5858ea0782c181e9d4297c49405c28019b8
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-12-05 15:10:09 +0100

    Simpler serialization of Date

--- Rides/src/main/scala/rides/adapters/persistence/RidesFileSystemRepositoryAdapter.scala
+++ Rides/src/main/scala/rides/adapters/persistence/RidesFileSystemRepositoryAdapter.scala
@@ -12,51 +12,18 @@ import shared.ports.persistence.exceptions.DuplicateIdException
 class RidesFileSystemRepositoryAdapter(db: FileSystemDatabase)
     extends RidesRepository:
 
-  private case class SerializableRide(
-      id: RideId,
-      eBikeId: EBikeId,
-      username: Username,
-      start: Long,
-      end: Option[Long]
-  ):
-    def toRide(): Ride =
-      Ride(id, eBikeId, username, Date(start), end.map(Date(_)))
-
-  extension (r: Ride)
-    private def toSerializableRide(): SerializableRide =
-      SerializableRide(
-        r.id,
-        r.eBikeId,
-        r.username,
-        r.start.getTime(),
-        r.end.map(_.getTime())
-      )
+  given ReadWriter[Date] =
+    readwriter[Long].bimap(
+      date => date.getTime(),
+      long => Date(long)
+    )
 
   given ReadWriter[Username] = ReadWriter.derived
   given ReadWriter[EBikeId] = ReadWriter.derived
   given ReadWriter[RideId] = ReadWriter.derived
-  private given ReadWriter[SerializableRide] = ReadWriter.derived
+  given ReadWriter[Ride] = ReadWriter.derived
 
-  private val repo =
-    FileSystemRepositoryAdapter[RideId, SerializableRide](db, "rides")
+  private[RidesFileSystemRepositoryAdapter] val repo =
+    FileSystemRepositoryAdapter[RideId, Ride](db, "rides")
 
-  override def getAll(): Iterable[Ride] =
-    repo.getAll().map(_.toRide())
-
-  override def insert(
-      id: RideId,
-      entity: Ride
-  ): Either[DuplicateIdException, Unit] =
-    repo.insert(id, entity.toSerializableRide())
-
-  override def update(
-      id: RideId,
-      f: Ride => Ride
-  ): Either[NotInRepositoryException, Ride] =
-    repo.update(id, r => f(r.toRide()).toSerializableRide()).map(_.toRide())
-
-  override def find(id: RideId): Option[Ride] =
-    repo.find(id).map(_.toRide())
-
-  override def delete(id: RideId): Either[NotInRepositoryException, Unit] =
-    repo.delete(id)
+  export repo.*
