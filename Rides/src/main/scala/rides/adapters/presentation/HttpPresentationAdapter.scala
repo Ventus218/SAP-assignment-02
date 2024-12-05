d2722582e609576dad5270b8a2ff1e37c9ec8fb7
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2024-12-05 17:41:07 +0100

    Simpler Date marshalling

--- Rides/src/main/scala/rides/adapters/presentation/HttpPresentationAdapter.scala
+++ Rides/src/main/scala/rides/adapters/presentation/HttpPresentationAdapter.scala
@@ -8,11 +8,12 @@ import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.model.StatusCodes.*
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json.RootJsonFormat
+import spray.json.*
+import spray.json.JsonWriter.func2Writer
+import spray.json.JsonReader.func2Reader
 import spray.json.DefaultJsonProtocol.*
 import rides.domain.model.*
 import rides.ports.RidesService
-import rides.adapters.presentation.DateMarshalling.*
 import rides.adapters.presentation.dto.*
 import rides.domain.errors.*
 import rides.domain.errors.UserOrEBikeAlreadyOnARide.*
@@ -20,6 +21,10 @@ import rides.domain.errors.UserOrEBikeDoesNotExist.*
 
 object HttpPresentationAdapter:
 
+  given JsonFormat[Date] = jsonFormat(
+    func2Reader(js => Date(js.asInstanceOf[JsNumber].value.toLong)),
+    func2Writer[Date](date => JsNumber(date.getTime()))
+  )
   given RootJsonFormat[Username] = jsonFormat1(Username.apply)
   given RootJsonFormat[EBikeId] = jsonFormat1(EBikeId.apply)
   given RootJsonFormat[RideId] = jsonFormat1(RideId.apply)
